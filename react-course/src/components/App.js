1c6fbf15453c2696b77150d708e4d0df61bea9b0
kevinavignon <kavignonpro@gmail.com>
2019-11-19 13:53:14 +0100

    Random Meme generator

--- react-course/src/components/App.js
+++ react-course/src/components/App.js
@@ -1,35 +1,15 @@
-import React, { Component } from "react"
+import React from 'react'
+import Header from '../components/Header'
+import MemeGenerator from '../components/MemeGenerator'
 
-class App extends Component {
-     constructor() {
-          super()
-          this.state = {
-               name: "",
-               surname: ""
-          }
-          this.handleChange = this.handleChange.bind(this)
-     }
+function App() {
+     return (
+          <div>
 
-     handleChange(event) {
-          this.setState({ [event.target.name]: event.target.value })
-     }
-
-     render() {
-          let isValidName = this.state.name.length > 3 ? "name is valid" : "name is too short"
-          let isValidSurname = this.state.surname.length > 4 ? "name is valid" : "name is too short"
-          return (
-               <div>
-                    <p>Le nom est : {this.state.name}</p>
-                    <p>Le prénom est : {this.state.surname}</p>
-                    <form>
-                         <input name="name" type="text" placeholder="name" onChange={this.handleChange} />
-                         <input name="surname" type="text" placeholder="surname" onChange={this.handleChange} />
-                    </form>
-                    <p>{isValidName}</p>
-                    <p>{isValidSurname}</p>
-               </div>
-          )
-     }
+               <Header />
+               <MemeGenerator />
+          </div>
+     )
 }
 
 export default App
